5a7ad329b46e914e2ca2332821cfecceb608bf80
laracamarino <laracamarino@gmail.com>
2022-04-21 19:40:15 -0300

    Informações Básicas do Quizz

--- assets/scripts.js
+++ assets/scripts.js
@@ -8,3 +8,35 @@ function irCriarQuizz() {
 function abrirConteudoPergunta(icone) {
     icone.parentNode.parentNode.querySelector(".conteudo-pergunta").classList.toggle("escondido");
 }
+
+function validacaoInformacoesBasicas() {
+    let tituloQuizz = document.getElementById("titulo-quizz").value;
+    let tituloImagem = document.getElementById("titulo-imagem").value;
+    let quantidadePerguntas = document.getElementById("quantidade-perguntas").value;
+    let quantidadeNiveis = document.getElementById("quantidade-niveis").value;
+
+    if(tituloQuizz.length < 20 || tituloQuizz.length > 65) {
+        alert("O título deve conter entre 20 e 65 caracteres.");
+    }
+    if(verificaUrl(tituloImagem) === false) {
+        alert("O endereço da imagem não é válido");
+    }
+    if(quantidadePerguntas < 3) {
+        alert("O quizz deve conter no mínimo 3 perguntas.");
+    }
+    if(quantidadeNiveis < 2) {
+        alert("O quizz deve conter no mínimo 2 níveis.");
+    }
+
+}
+
+function verificaUrl(url) {
+    const pattern = /^https:\/\//i;
+
+    if (pattern.test(url)) {
+        return true
+    }
+    else {
+        return false;
+    }
+}
